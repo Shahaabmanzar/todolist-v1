daf98854fba83515590c2dec43d3598bc367b408
Shahaab Manzar <mshahaabmanzar@gmail.com>
2020-12-29 01:03:44 +0530

    what day is it

--- app.js
+++ app.js
@@ -2,15 +2,42 @@ const express=require("express");
 const bodyParser=require("body-parser");
 
 const app=express();
+app.set("view engine", "ejs");
+
 
 app.get("/",function(req,res){
 
     var today = new Date();
-    if(today.getDay()===6||today.getDay()===0){
-        res.send("Yay its the weekend");
-    }else{
-        res.send("boo! i have to work");
+    var currentDay=today.getDay();
+    var day="";
+
+    switch (currentDay) {
+        case 0:
+            day="sunday"
+            break;
+        case 1:
+            day="monday"
+            break;
+            case 2:
+            day="tuesday"
+            break;
+            case 3:
+            day="wednesday"
+            break;
+            case 4:
+            day="thursday"
+            break;
+            case 5:
+            day="friday"
+            break;
+            case 6:
+            day="saturday"
+            break;
+
+        default:
+            console.log("ERROR!current day is equal to:" + currentDay);
     }
+            res.render("list",{kindOfDay:day});
 });
 
 app.listen(3000,function(){
