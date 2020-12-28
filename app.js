84400d3238d43d81954f84ec8f6ca391595a9c01
Shahaab Manzar <mshahaabmanzar@gmail.com>
2020-12-29 01:42:00 +0530

    commit

--- app.js
+++ app.js
@@ -2,42 +2,27 @@ const express=require("express");
 const bodyParser=require("body-parser");
 
 const app=express();
+var items=[];
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
 
     var today = new Date();
-    var currentDay=today.getDay();
-    var day="";
 
-    switch (currentDay) {
-        case 0:
-            day="sunday"
-            break;
-        case 1:
-            day="monday"
-            break;
-            case 2:
-            day="tuesday"
-            break;
-            case 3:
-            day="wednesday"
-            break;
-            case 4:
-            day="thursday"
-            break;
-            case 5:
-            day="friday"
-            break;
-            case 6:
-            day="saturday"
-            break;
+    var options={
+        weekday: "long",
+        day:"numeric",
+        month:"long"
+    };
+    var day=today.toLocaleDateString("en-US",options);
+            res.render("list",{kindOfDay:day, newListItem:items});
+});
 
-        default:
-            console.log("ERROR!current day is equal to:" + currentDay);
-    }
-            res.render("list",{kindOfDay:day});
+app.post("/", function(req,res){
+    var item=req.body.navItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 app.listen(3000,function(){
